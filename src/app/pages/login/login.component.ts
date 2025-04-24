f511da14e13c3b4f7531d0ec95ba3646866e9d0d
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-04-24 22:58:22 +0530

    Login component front end changes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+//import { Router } from 'express';
 // import { console } from 'inspector';
 // import { error } from 'console';
 
@@ -15,7 +17,7 @@ export class LoginComponent implements OnInit{
     password:'',
   };
 
-  constructor(private snack: MatSnackBar, private login:LoginService){}
+  constructor(private snack: MatSnackBar, private login:LoginService, private router: Router){}
 
   ngOnInit(): void {
     
@@ -44,10 +46,35 @@ export class LoginComponent implements OnInit{
       (data: any) =>{
         console.log('sucess');
         console.log(data);
+
+
+        //login
+        this.login.loginUser(data.token);
+        this.login.getCurrentUser().subscribe((user: any) =>{
+          this.login.setUser(user);
+          console.log(user);
+
+          if(this.login.getUserRole() == 'ADMIN'){
+         //   window.location.href= '/admin';
+         this.router.navigate(['admin'])
+          }
+          
+          else if(this.login.getUserRole() == 'NORMAL'){
+              //window.location.href= '/user-dashboard'; 
+              this.router.navigate(['user-dashboard'])  
+          } 
+          else{
+            this.login.logout;
+          }
+
+        });
       },
       (error)=>{
         console.log('Error!');
         console.log(error);
+        this.snack.open('Invalid Details !! Try again', '', {
+          duration: 3000,
+        });
       }
     );
   }
---
+++ src/app/services/normal.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NormalGuard implements CanActivate {
+
+  constructor(private login: LoginService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.login.isLoggedIn() && this.login.getUserRole() == 'NORMAL') {
+      return true;
+    }
+
+    this.router.navigate(['login']);
+    return false;
+  }
+}
