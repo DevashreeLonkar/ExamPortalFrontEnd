9575a9449e87b885f8b76ae073f81c61e706369b
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-04-29 22:28:32 +0530

    Add Category and View Category

--- src/app/pages/admin/view-categories/view-categories.component.ts
+++ src/app/pages/admin/view-categories/view-categories.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../../services/category.service';
+import Swal from 'sweetalert2';
+
+interface Category {  // Define the interface here
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-view-categories',
   templateUrl: './view-categories.component.html',
-  styleUrl: './view-categories.component.css'
+  styleUrls: ['./view-categories.component.css']
 })
-export class ViewCategoriesComponent {
+export class ViewCategoriesComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  constructor(private _category: CategoryService) {}
+
+  ngOnInit(): void {
+    this._category.categories().subscribe(
+      (data: any) => {
+        this.categories = data;
+        console.log(this.categories);
+      },
+      (error) => {
+        console.log(error);
+        Swal.fire('Error !!', 'Error in loading data', 'error');
+      }
+    );
+  }
 
 }
