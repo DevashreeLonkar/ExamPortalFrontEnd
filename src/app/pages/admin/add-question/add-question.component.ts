d1b59a1429c266c0b8ef0b08f3b65857f443f596
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-05-09 23:44:58 +0530

    add question

--- src/app/pages/admin/add-question/add-question.component.ts
+++ src/app/pages/admin/add-question/add-question.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-question',
   templateUrl: './add-question.component.html',
   styleUrl: './add-question.component.css'
 })
-export class AddQuestionComponent {
+export class AddQuestionComponent implements OnInit{
+
+  qId: any;
+  question={
+    quiz: {
+      qId: null
+    },
+    content: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    answer: '',
+  };
+
+  constructor(private _route:ActivatedRoute){}
+
+  ngOnInit(): void {
+   this.qId= this._route.snapshot.params['qid'];
+   this.question.quiz['qId']= this.qId;
+  }
 
 }
